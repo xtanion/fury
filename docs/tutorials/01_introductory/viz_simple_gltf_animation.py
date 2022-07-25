afead4cdd871df4ba90a889b49a2d3093436c3d7
xtanion <anandshivam54321@gmail.com>
2022-07-25 21:33:29 +0530

    Extracting animation data and returning nodes

---
+++ docs/tutorials/01_introductory/viz_simple_gltf_animation.py
@@ -0,0 +1,50 @@
+import numpy as np
+from fury import actor, window
+from fury.animation.timeline import Timeline
+from fury.animation.interpolator import LinearInterpolator
+from fury.gltf import glTF
+from fury.data import fetch_gltf, read_viz_gltf
+
+scene = window.Scene()
+
+showm = window.ShowManager(scene,
+                           size=(900, 768), reset_camera=False,
+                           order_transparent=True)
+showm.initialize()
+
+fetch_gltf('BoxAnimated', 'glTF')
+filename = read_viz_gltf('BoxAnimated')
+
+gltf_obj = glTF(filename)
+actors = gltf_obj.actors()
+transforms = gltf_obj.node_transform
+nodes = gltf_obj.nodes
+
+main_timeline = Timeline(actors, playback_panel=Timeline)
+
+for transform in transforms:
+    target_node = transform['node']
+    for i, node_list in enumerate(nodes):
+        if target_node in node_list:
+            timeline = Timeline(actors[i])
+
+            timeframes = transform['input']
+            transform = transform['output']
+
+            for time, node_tran in zip(timeframes, transform):
+                timeline.set_position(time[0], node_tran)
+
+            timeline.set_position_interpolator(LinearInterpolator)
+            main_timeline.add_timeline(timeline)
+        else:
+            main_timeline.add_static_actor(actors[i])
+
+def timer_callback(_obj, _event):
+    main_timeline.update_animation()
+    showm.render()
+
+
+# Adding the callback function that updates the animation
+showm.add_timer_callback(True, 10, timer_callback)
+
+showm.start()
