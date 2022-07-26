503cc23f4f4ac546dbbb6d97c209705b2e70f1f7
xtanion <anandshivam54321@gmail.com>
2022-07-26 12:45:37 +0530

    Gets property of node transformation

--- docs/tutorials/01_introductory/viz_simple_gltf_animation.py
+++ docs/tutorials/01_introductory/viz_simple_gltf_animation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 from fury import actor, window
 from fury.animation.timeline import Timeline
 from fury.animation.interpolator import LinearInterpolator
@@ -20,24 +21,38 @@ actors = gltf_obj.actors()
 transforms = gltf_obj.node_transform
 nodes = gltf_obj.nodes
 
-main_timeline = Timeline(actors, playback_panel=Timeline)
+print(len(actors))
+
+main_timeline = Timeline(playback_panel=True)
 
 for transform in transforms:
     target_node = transform['node']
     for i, node_list in enumerate(nodes):
         if target_node in node_list:
+            print(i)
             timeline = Timeline(actors[i])
 
             timeframes = transform['input']
-            transform = transform['output']
+            transforms = transform['output']
+            prop = transform['property']
+            print(prop)
 
-            for time, node_tran in zip(timeframes, transform):
-                timeline.set_position(time[0], node_tran)
+            for time, node_tran in zip(timeframes, transforms):
+                
+                if prop == 'rotation':
+                    rot = Rotation.from_quat(node_tran)
+                    rot_euler = rot.as_euler('xyz', degrees=True)
+                    timeline.set_rotation(time[0], rot_euler)
+                if prop == 'translation':
+                    print('chaning position')
+                    timeline.set_position(time[0], node_tran)
 
             timeline.set_position_interpolator(LinearInterpolator)
+            timeline.set_rotation_interpolator(LinearInterpolator)
             main_timeline.add_timeline(timeline)
         else:
             main_timeline.add_static_actor(actors[i])
+
 
 def timer_callback(_obj, _event):
     main_timeline.update_animation()
