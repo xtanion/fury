30962fae988fb4e068074824bde6b4a1ff5b10ff
xtanion <anandshivam54321@gmail.com>
2022-07-28 22:28:50 +0530

    updated the example

--- docs/tutorials/01_introductory/viz_simple_gltf_animation.py
+++ docs/tutorials/01_introductory/viz_simple_gltf_animation.py
@@ -1,8 +1,8 @@
-import numpy as np
 from scipy.spatial.transform import Rotation
-from fury import actor, window
+from fury import window
 from fury.animation.timeline import Timeline
-from fury.animation.interpolator import LinearInterpolator
+from fury.animation.interpolator import (LinearInterpolator, StepInterpolator,
+                                         CubicSplineInterpolator)
 from fury.gltf import glTF
 from fury.data import fetch_gltf, read_viz_gltf
 
@@ -13,19 +13,22 @@ showm = window.ShowManager(scene,
                            order_transparent=True)
 showm.initialize()
 
-fetch_gltf('BoxAnimated', 'glTF')
-filename = read_viz_gltf('BoxAnimated')
+fetch_gltf('InterpolationTest', 'glTF')
+filename = read_viz_gltf('InterpolationTest')
 
 gltf_obj = glTF(filename)
 actors = gltf_obj.actors()
 
-# simplyfy the example, add the followingcode to a function indide the glTF 
+# simplyfy the example, add the followingcode to a function indide the glTF
 # object.
 transforms = gltf_obj.node_transform
 nodes = gltf_obj.nodes
 
-print(len(actors))
-print(nodes)
+interpolator = {
+    'LINEAR': LinearInterpolator,
+    'STEP': StepInterpolator,
+    'CUBICSPLINE': LinearInterpolator
+}
 
 main_timeline = Timeline(playback_panel=True)
 
@@ -33,27 +36,29 @@ for transform in transforms:
     target_node = transform['node']
     for i, node_list in enumerate(nodes):
         if target_node in node_list:
-            print(i)
             timeline = Timeline()
             timeline.add_actor(actors[i])
 
             timeframes = transform['input']
             transforms = transform['output']
             prop = transform['property']
-            print(prop)
+            interp = interpolator.get(transform['interpolation'])
 
             for time, node_tran in zip(timeframes, transforms):
 
                 if prop == 'rotation':
                     rot = Rotation.from_quat(node_tran)
                     rot_euler = rot.as_euler('xyz', degrees=True)
+                    # print(rot_euler)
                     timeline.set_rotation(time[0], rot_euler)
                 if prop == 'translation':
-                    print(time[0])
                     timeline.set_position(time[0], node_tran)
+                if prop == 'scale':
+                    timeline.set_scale(time[0], node_tran)
 
-            timeline.set_position_interpolator(LinearInterpolator)
-            timeline.set_rotation_interpolator(LinearInterpolator)
+            timeline.set_position_interpolator(interp)
+            timeline.set_rotation_interpolator(interp)
+            timeline.set_scale_interpolator(interp)
             main_timeline.add_timeline(timeline)
         else:
             main_timeline.add_static_actor(actors[i])
